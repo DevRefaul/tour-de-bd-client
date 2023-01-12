16f9dfc0f5007145a5ec3df9d37a5073012444d8
DevRefaul <devrefaul@gmail.com>
2023-01-12 19:10:33 +0600

    all icons link problem solved in bottom footer andnow they will open in another page

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -119,9 +119,11 @@ const Footer = () => {
               </svg>
               <span className="sr-only">Facebook page</span>
             </a>
-            <Link
-              to="#https://instagram.com"
+            <a
+              href="https://instagram.com"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+              target="_blank"
+              rel="noreferrer"
             >
               <svg
                 className="w-5 h-5"
@@ -136,10 +138,12 @@ const Footer = () => {
                 ></path>
               </svg>
               <span className="sr-only">Instagram page</span>
-            </Link>
-            <Link
-              to="https://twitter.com"
+            </a>
+            <a
+              href="https://twitter.com"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+              target="_blank"
+              rel="noreferrer"
             >
               <svg
                 className="w-5 h-5"
@@ -150,10 +154,12 @@ const Footer = () => {
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
               </svg>
               <span className="sr-only">Twitter page</span>
-            </Link>
-            <Link
-              to="https://github.com/devrefaul"
+            </a>
+            <a
+              href="https://github.com/devrefaul"
               className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+              target="_blank"
+              rel="noreferrer"
             >
               <svg
                 className="w-5 h-5"
@@ -168,7 +174,7 @@ const Footer = () => {
                 ></path>
               </svg>
               <span className="sr-only">GitHub account</span>
-            </Link>
+            </a>
           </div>
         </div>
       </footer>
